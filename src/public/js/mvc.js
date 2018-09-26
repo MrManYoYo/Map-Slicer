bacc3b58f7bbb1804edfcda6fbc7dc242eb32362
MrManYoYo <sky_variant@126.com>
2018-09-26 14:34:25 +0800

    listening array

--- src/public/js/mvc.js
+++ src/public/js/mvc.js
@@ -1,3 +1,4 @@
+// 监听对象属性变化
 const observe = obj => {
   if (!obj || typeof obj != 'object') {
     return
@@ -15,6 +16,21 @@ const definePro = (obj, key, value) => {
     set: function (newval) {
       console.log('检测变化', newval);
       value = newval;
+    }
+  })
+}
+
+// 监听数组变化
+const subscribeArray = () => {
+  const arr = []
+  return new Proxy(arr, {
+    get: function (target, key, receiver) {
+      return target[key]
+    },
+    set: function (target, key, value, receiver) {
+      target[key] = value;
+      console.log(`修改了key:${key},新值：${value}, arr: ${arr}`)
+      return true
     }
   })
 }
